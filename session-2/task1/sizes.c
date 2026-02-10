0ffb909283f6ee2f3e833c6b1a819ef3615fbd58
Lucas Parbott Clowe <cmnp0658@leeds.ac.uk>
2026-02-10 10:58:18 +0000

    worksheet and session 1

--- session-2/task1/sizes.c
+++ session-2/task1/sizes.c
@@ -10,11 +10,17 @@ int main( void ) {
 
     char testChar;
     int testInt;
+    short int testShortInt;
+    long int testLongInt;
     float testFloat;
+    double testDouble;
 
     printf("Char size is %ld bytes\n",sizeof(testChar));
     printf("Int size is %ld bytes\n",sizeof(testInt));
+    printf("Short Int size is %ld bytes\n",sizeof(testShortInt));
+    printf("Long Int size is %ld bytes\n",sizeof(testLongInt));
     printf("Float size is %ld bytes\n",sizeof(testFloat));
+    printf("Double size is %ld bytes\n",sizeof(testDouble));
 
     return 0;
 }
--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,37 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Lucas Parbott Clowe
+ * ID: 201972343
  */
 
  int main( void ) {
 
-    // define and initialise variables for the problem data 
+    // define and initialise variables for the problem data
+    float salary = 36250;
+    float national_insurance = 0.08;
+    float tax_rate = 0.15;
 
     // calculate the deductions and final take-home salary
+    float national_insurance_contribution = salary * national_insurance;
+
+    // if the salary with NI taken is greater than 12500 then calculate the tax on the amount over
+    if ((salary - national_insurance_contribution) > 12500) {
+       float tax_contribution = (salary - national_insurance_contribution) * tax_rate;
+    }
+
+    // else set the tax to 0
+    else {
+      float tax_contribution = 0;
+    }
+    
+    float take_home_salary = salary - national_insurance_contribution - tax_contribution;
 
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+    printf("Salary £%f\n", salary);
+    printf("NI contribution £%f\n", national_insurance_contribution);
+    printf("Tax contribution £%f\n",tax_contribution);
+    printf("Take home salary £%f\n",take_home_salary);
 
     return 0;
  }
