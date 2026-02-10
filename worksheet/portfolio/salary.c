de355bcf611dd0979da8d6835658e6997c9c0701
Lucas Parbott Clowe <cmnp0658@leeds.ac.uk>
2026-02-10 20:34:42 +0000

    completed worksheet

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -13,27 +13,23 @@
     float salary = 36250;
     float national_insurance = 0.08;
     float tax_rate = 0.15;
+    float tax_contribution = 0;
 
     // calculate the deductions and final take-home salary
     float national_insurance_contribution = salary * national_insurance;
 
-    // if the salary with NI taken is greater than 12500 then calculate the tax on the amount over
+    // if the salary with NI taken is greater than 12500 then calculate the tax on the amount over else set the tax to 0
     if ((salary - national_insurance_contribution) > 12500) {
-       float tax_contribution = (salary - national_insurance_contribution) * tax_rate;
+       tax_contribution = (salary - 12500 - national_insurance_contribution) * tax_rate;
     }
 
-    // else set the tax to 0
-    else {
-      float tax_contribution = 0;
-    }
-    
     float take_home_salary = salary - national_insurance_contribution - tax_contribution;
 
     // Use only these print statement with appropriate formatting and variable names
-    printf("Salary £%f\n", salary);
-    printf("NI contribution £%f\n", national_insurance_contribution);
-    printf("Tax contribution £%f\n",tax_contribution);
-    printf("Take home salary £%f\n",take_home_salary);
+    printf("Salary £%.2f\n", salary);
+    printf("NI contribution £%.2f\n", national_insurance_contribution);
+    printf("Tax contribution £%.2f\n",tax_contribution);
+    printf("Take home salary £%.2f\n",take_home_salary);
 
     return 0;
  }
